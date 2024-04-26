65cc5add869b7a8f90d6b709018d40917ccff218
BHImran <imran689690@gmail.com>
2024-04-27 01:27:15 +0600

    my frist commit

---
+++ main.py
@@ -0,0 +1,10 @@
+bic={
+    'name':'imran',
+    'roll':'689690',
+    'age':'21'
+}
+# print(bic['name'])
+
+for i in bic:
+    # print(i)
+     print(bic[i])
---
+++ problem.py
@@ -0,0 +1,18 @@
+
+                # item exchange
+
+# type=>1
+# x=10;
+# y=5;
+# m=x;
+# x=y;
+# print(x)
+# y=m;
+# print(y)
+
+# type=>
+# x=20;
+# y=50;
+# x,y=y,x;
+# print(x)
+# print(y)
